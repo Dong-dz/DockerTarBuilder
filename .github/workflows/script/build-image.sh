d7b2636e5d7689f2eee9bc86a41a09d26332bdd7
Dong <42765599+Dong-dz@users.noreply.github.com>
2024-09-29 22:49:56 +0800

    Update build-image.sh

--- .github/workflows/script/build-image.sh
+++ .github/workflows/script/build-image.sh
@@ -1,7 +1,7 @@
-cat >> /etc/apt/sources.list.d/ros-latest.list << EOF
+sudo cat >> /etc/apt/sources.list.d/ros-latest.list << EOF
 deb http://mirrors.ustc.edu.cn/ros/ubuntu/ focal main
 EOF
-apt-get update 
+sudo apt-get update 
 docker build -t ros:noetic-dong . 
 docker save -o ros.tar ros:noetic-dong
 tar -czf x86-64-image.tar.gz ros.tar
