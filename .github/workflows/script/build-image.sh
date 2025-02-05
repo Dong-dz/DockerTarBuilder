be0c0603d444e1e15b8a4f791fe5130b4cfb6b47
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-02-05 11:16:17 +0800

    Update build-image.sh

--- .github/workflows/script/build-image.sh
+++ .github/workflows/script/build-image.sh
@@ -1,7 +1,7 @@
 #sudo cat >> /etc/apt/sources.list.d/ros-latest.list << EOF
 #deb http://mirrors.ustc.edu.cn/ros/ubuntu/ focal main
 #EOF
-sudo apt-get update 
-docker build -t ros:noetic-dong . 
-docker save -o ros.tar ros:noetic-dong
-tar -czf x86-64-image.tar.gz ros.tar
+#sudo apt-get update 
+docker build -t deepclaude:20250205 . 
+docker save -o deepclaude.tar deepclaude:20250205
+tar -czf x86-64-image.tar.gz deepclaude.tar
