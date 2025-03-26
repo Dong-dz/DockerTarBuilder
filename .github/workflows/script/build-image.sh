459c74af9c458ab137bf8818e0f2b1678da20bc3
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-03-26 20:51:43 +0800

    Update build-image.sh

--- .github/workflows/script/build-image.sh
+++ .github/workflows/script/build-image.sh
@@ -3,8 +3,13 @@
 #EOF
 #sudo apt-get update 
 
-git clone https://github.com/getasterisk/deepclaude.git
-cd deepclaude
-docker build -t deepclaude:20250205 . 
-docker save -o deepclaude-amd64.tar deepclaude:20250205
-tar -czf x86-64-image.tar.gz deepclaude-amd64.tar
+# git clone https://github.com/getasterisk/deepclaude.git
+# cd deepclaude
+# docker build -t deepclaude:20250205 . 
+# docker save -o deepclaude-amd64.tar deepclaude:20250205
+# tar -czf x86-64-image.tar.gz deepclaude-amd64.tar
+
+git clone https://oauth2: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@https://github.com/Dong-dz/DockerTarBuilder.git
+cd .github/workflows/script
+docker build -t nginx:1.25_logrotate . 
+docker save -o nginx.tar nginx:1.25_logrotate 
