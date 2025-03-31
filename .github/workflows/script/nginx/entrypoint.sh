b9fbe53fb8af89df8a655b89f488a46a29db7493
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-03-31 16:35:50 +0800

    Update entrypoint.sh

--- .github/workflows/script/nginx/entrypoint.sh
+++ .github/workflows/script/nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 # 启动cron服务并重定向日志输出
 cron
 # 将nginx日志输出到标准输出（可选）
-ln -sf /dev/stdout /etc/nginx/logs/access.log
-ln -sf /dev/stderr /etc/nginx/logs/error.log
+#ln -sf /dev/stdout /etc/nginx/logs/access.log
+#ln -sf /dev/stderr /etc/nginx/logs/error.log
 # 前台运行nginx
 exec nginx -g "daemon off;"
