402e9c3ffc52e4b7a69d286ad47fab9d74bfbe1b
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-03-31 15:51:04 +0800

    Update build-image.sh

--- .github/workflows/script/nginx/build-image.sh
+++ .github/workflows/script/nginx/build-image.sh
@@ -1,5 +1,5 @@
-# docker build -t nginx:1.25_logrotate . 
+docker build -t nginx:1.25_logrotate . 
 # docker save -o nginx.tar nginx:1.25_logrotate 
 
-docker buildx build -f ./Dockerfile --platform linux/amd64,linux/arm64 -t nginx:1.25_logrotate .
+#docker buildx build -f ./Dockerfile --platform linux/amd64,linux/arm64 -t nginx:1.25_logrotate .
 docker save -o nginx.tar nginx:1.25_logrotate 
