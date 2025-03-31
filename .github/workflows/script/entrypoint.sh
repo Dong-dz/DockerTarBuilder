42fed006c8e0010f66e4de4f87d7ffb712c114a0
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-03-31 13:57:21 +0800

    Delete .github/workflows/script/entrypoint.sh

--- .github/workflows/script/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# 启动cron服务并重定向日志输出
-cron
-# 将nginx日志输出到标准输出（可选）
-ln -sf /dev/stdout /var/log/nginx/access.log
-ln -sf /dev/stderr /var/log/nginx/error.log
-# 前台运行nginx
-exec nginx -g "daemon off;"
