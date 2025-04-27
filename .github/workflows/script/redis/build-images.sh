1e0899f846ea390408cec965e5d5f05704fc3cca
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-04-27 17:12:31 +0800

    Delete .github/workflows/script/redis/build-images.sh

--- .github/workflows/script/redis/build-images.sh
+++
@@ -1,5 +0,0 @@
-docker build  --platform linux/arm64 --tag redis_dump:20250427_arm .
-docker build  --platform linux/amd64 --tag redis_dump:20250427_x86 .
-
-docker save -o redisdump_arm.tar redis_dump:20250427_arm
-docker save -o redisdump_x86.tar redis_dump:20250427_x86
