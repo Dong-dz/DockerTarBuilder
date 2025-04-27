887801140a6b58c87b8f88ee43452ab4669816c1
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-04-27 17:07:56 +0800

    Update build-images.sh

--- .github/workflows/script/redis/build-images.sh
+++ .github/workflows/script/redis/build-images.sh
@@ -1,2 +1,5 @@
-docker build  --platform linux/arm64 --tag redis_dump:20250427 .
-docker save -o redisdump.tar redis_dump:20250427
+docker build  --platform linux/arm64 --tag redis_dump:20250427_arm .
+docker build  --platform linux/amd64 --tag redis_dump:20250427_x86 .
+
+docker save -o redisdump_arm.tar redis_dump:20250427_arm
+docker save -o redisdump_x86.tar redis_dump:20250427_x86
