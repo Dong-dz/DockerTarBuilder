de83637900fd32560143f33367ec462c55a828a8
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-04-27 17:05:39 +0800

    Update build-image.sh

--- .github/workflows/script/nginx/build-image.sh
+++ .github/workflows/script/nginx/build-image.sh
@@ -1,9 +1,9 @@
-# docker build -t nginx:1.25_logrotate . 
+docker build -t nginx:1.25_logrotate . 
 # docker save -o nginx.tar nginx:1.25_logrotate 
       
 # docker buildx build -f ./Dockerfile --platform linux/amd64,linux/arm64 -t nginx:1.25_logrotate .
-# docker save -o nginx.tar nginx:1.25_logrotate 
+docker save -o nginx.tar nginx:1.25_logrotate 
 
 
-docker build  --platform linux/arm64 --tag redis_dump:20250427 .
-docker save -o redisdump.tar redis_dump:20250427
+#docker build  --platform linux/arm64 --tag redis_dump:20250427 .
+#docker save -o redisdump.tar redis_dump:20250427
