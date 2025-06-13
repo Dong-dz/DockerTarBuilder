b5d0db34131812e9c3bbc3ddefd754172b633311
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-06-13 15:49:29 +0800

    Update build-image.sh

--- .github/workflows/script/ascend-exporter/build-image.sh
+++ .github/workflows/script/ascend-exporter/build-image.sh
@@ -4,11 +4,15 @@ ls
 cd ascend-npu-exporter/build/
 ls 
 
+echo "构建exporter"
 chmod +x build.sh
 bash build.sh
 
+echo "构建镜像"
+cd ../output
 docker build -t ascend:master .
 docker save -o ascend.tar ascend:master
 mv  ascend.tar  .. 
+
 ls 
 ls ..
