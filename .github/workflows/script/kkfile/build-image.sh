d30a87a72bc5a97972237a12fa425d36406d417e
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-07-10 16:03:57 +0800

    Update build-image.sh

--- .github/workflows/script/kkfile/build-image.sh
+++ .github/workflows/script/kkfile/build-image.sh
@@ -2,16 +2,17 @@ git clone --branch v4.4.0 https://github.com/kekingcn/kkFileView.git
 ls 
 
 ls 
-505403623/public
+
 cd kkFileView/docker/kkfileview-base/
-docker build --platform linux/amd64 -t kkfileview-base:4.4.0 .
+docker build --platform linux/amd64 -t 505403623/public/kkfileview-base:4.4.0 .
 
 cd ../..
 
 mvn -B package -Dmaven.test.skip=true --file pom.xml
 tar -czvf  kkFileViewjar.tar server/target/kkFileView-*.tar.gz 
 
-docker save -o kkfileview-base.tar kkfileview-base:4.4.0
+docker save -o kkfileview-base.tar 505403623/public/kkfileview-base:4.4.0
+docker push 505403623/public/kkfileview-base:4.4.0
 
 mv  kkfileview-base.tar  .. 
 mv  kkFileViewjar.tar ..
