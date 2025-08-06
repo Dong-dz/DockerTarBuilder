a0b8bf0c94df33a0d7af07291402f9d345b3664c
Dong <42765599+Dong-dz@users.noreply.github.com>
2025-08-06 18:08:38 +0800

    Update images.sh

--- .github/workflows/script/cmdb/backend/images.sh
+++ .github/workflows/script/cmdb/backend/images.sh
@@ -1,1 +1,22 @@
-1
+go env -w GO111MODULE="auto"
+
+git clone https://github.com/TencentBlueKing/bk-cmdb configcenter
+
+cd configcenter
+go mod tidy
+
+cd configcenter/src
+make server
+make package
+
+cd configcenter/src/bin/pub/
+tar -xvf  cmdb.tar.gz
+cd cmdb
+./image.sh
+
+# ls 
+# bash build_image.sh
+# docker save -o milvus-cdc.tar docker.io/library/milvus-cdc:main-a54f09b
+# mv  milvus-cdc.tar  .. 
+# ls 
+# ls ..
