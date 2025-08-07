82a2d2aef9482715c00afaa9c5a61452d34a53ac
505403623 <505403623@qq.com>
2025-08-07 09:08:22 +0800

    cmdb

--- .github/workflows/script/cmdb/build-image.sh
+++ .github/workflows/script/cmdb/build-image.sh
@@ -1,22 +1,24 @@
-go env -w GO111MODULE="auto"
-
+echo "下载源码包"
 git clone https://github.com/TencentBlueKing/bk-cmdb configcenter
 
+echo "安装依赖"
+pwd 
+ls 
+go env -w GO111MODULE="auto"
 cd configcenter
 go mod tidy
 
+echo "编译"
+pwd
+ls
 cd src
 make server
 make package
 
+echo "构建镜像"
+pwd 
+ls 
 cd bin/pub/
 tar -xvf  cmdb.tar.gz
 cd cmdb
 ./image.sh
-
-# ls  sfswefrsfs 
-# bash build_image.sh
-# docker save -o milvus-cdc.tar docker.io/library/milvus-cdc:main-a54f09b
-# mv  milvus-cdc    .tar  .. 
-# ls 
-# ls ..
